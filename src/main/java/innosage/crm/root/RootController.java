1a9a6593a9593f1d4a8b597214a07bd8192ca010
구환준/모건 <koojun99@naver.com>
2024-05-02 16:58:56 +0900

    Ci/#5-CI/CD 파이프라인 구축

---
+++ src/main/java/innosage/crm/root/RootController.java
@@ -0,0 +1,14 @@
+package innosage.crm.root;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm healthy!";
+    }
+
+}
