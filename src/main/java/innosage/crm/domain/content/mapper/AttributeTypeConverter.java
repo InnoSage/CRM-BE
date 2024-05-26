52087ed01e41507984cf7e781dc6d0141fdc3bc1
구환준/모건 <koojun99@naver.com>
2024-05-26 15:48:17 +0900

    Hotfix: DATE 속성 Content 형변환

--- src/main/java/innosage/crm/domain/content/mapper/AttributeTypeConverter.java
+++ src/main/java/innosage/crm/domain/content/mapper/AttributeTypeConverter.java
@@ -2,6 +2,8 @@ package innosage.crm.domain.content.mapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
@@ -25,6 +27,13 @@ public class AttributeTypeConverter implements AttributeConverter<Object, String
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
+    static {
+        JavaTimeModule module = new JavaTimeModule();
+        objectMapper.registerModule(module);
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+
+
     @Override
     public String convertToDatabaseColumn(Object attribute) {
         try {
@@ -37,6 +46,7 @@ public class AttributeTypeConverter implements AttributeConverter<Object, String
     @Override
     public Object convertToEntityAttribute(String dbData) {
         try {
+            if (isValidJson(dbData)) {
             JsonNode jsonNode = objectMapper.readTree(dbData);
             if (jsonNode.isInt()) {
                 return jsonNode.intValue();
@@ -56,8 +66,20 @@ public class AttributeTypeConverter implements AttributeConverter<Object, String
             } else {
                 throw new IllegalArgumentException("Unsupported JSON value: " + jsonNode);
             }
+            } else {
+                return dbData;
+            }
         } catch (IOException e) {
             throw new IllegalArgumentException("Error reading JSON string: " + e.getMessage(), e);
+        }
+    }
+
+    private boolean isValidJson(String dbData) {
+        try {
+            objectMapper.readTree(dbData);
+            return true;
+        } catch (IOException e) {
+            return false;
         }
     }
 }
