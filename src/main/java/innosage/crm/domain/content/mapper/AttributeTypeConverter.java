3ac4a659bb87e13d115a000631bb0ad4fc9e3eec
구환준/모건 <koojun99@naver.com>
2024-05-28 00:48:06 +0900

    Hotfix: 날짜 타입 데이터 삽입 개선

--- src/main/java/innosage/crm/domain/content/mapper/AttributeTypeConverter.java
+++ src/main/java/innosage/crm/domain/content/mapper/AttributeTypeConverter.java
@@ -37,6 +37,10 @@ public class AttributeTypeConverter implements AttributeConverter<Object, String
     @Override
     public String convertToDatabaseColumn(Object attribute) {
         try {
+            if (attribute instanceof LocalDate) {
+                // LocalDate를 ISO_LOCAL_DATE 형식의 문자열로 변환
+                return ((LocalDate) attribute).format(DateTimeFormatter.ISO_LOCAL_DATE);
+            }
             return objectMapper.writeValueAsString(attribute);
         } catch (JsonProcessingException e) {
             throw new IllegalArgumentException("Error converting attribute to JSON string: " + e.getMessage(), e);
@@ -67,7 +71,13 @@ public class AttributeTypeConverter implements AttributeConverter<Object, String
                 throw new IllegalArgumentException("Unsupported JSON value: " + jsonNode);
             }
             } else {
+                try {
+                    // JSON이 아닌 경우 LocalDate로 변환을 시도
+                    return LocalDate.parse(dbData, DateTimeFormatter.ISO_LOCAL_DATE);
+                } catch (Exception e) {
+                    // 변환 실패 시 원본 문자열 반환
                 return dbData;
+                }
             }
         } catch (IOException e) {
             throw new IllegalArgumentException("Error reading JSON string: " + e.getMessage(), e);
